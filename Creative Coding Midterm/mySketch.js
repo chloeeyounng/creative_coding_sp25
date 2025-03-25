b0d9cbcf98dbee5d9fbc82a8f41e678071b17c83
Chloe Young <chloeyoung@Chloes-MacBook-Air.local>
2025-03-24 23:21:33 -0700

    Added Midterm Files

--- Creative Coding Midterm/mySketch.js
+++ Creative Coding Midterm/mySketch.js
@@ -11,10 +11,6 @@ function setup() {
   for (let i = 0; i < num; i++) {
     molds[i] = new Mold();
   }
-  describe(
-    "This sketch simulates behaviors of slime molds. Each slime mold object has position (x and y), traveling direction (r and heading angle) and sensor (in 3 directions: front, left, and forward). As a slime mold moves through the trail, it leaves a trace and the trail map is updated. In each simulation step, a slime mold senses the trail map (the pixel color value) and decides which direction to move and rotate.",
-    LABEL
-  );
 }
 
 function draw() {
